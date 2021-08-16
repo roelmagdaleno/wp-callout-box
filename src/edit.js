6570dd1f6383cd6a19fc0c3d975fe7994932a947
Roel Magdaleno <roelmagdaleno@gmail.com>
2021-08-16 10:10:40 -0500

    FEATURE: ÂCreate callout using presets (#6)

--- src/edit.js
+++ src/edit.js
@@ -1,7 +1,10 @@
 import {
+    Panel,
     PanelBody,
     PanelRow,
-    SelectControl
+    SelectControl,
+    RadioControl,
+    __experimentalRadio as Radio
 } from '@wordpress/components';
 
 import {
@@ -18,13 +21,15 @@ import {
 import icons from './icons.json';
 import types from './types.json';
 import Icon from './Icon';
+import presets from "./presets.json";
 
 const edit = ( { attributes, setAttributes } ) => {
     const {
         icon,
         type,
         method,
-        content
+        content,
+        preset
     } = attributes;
 
     const onChangeIcon = icon => {
@@ -43,53 +48,92 @@ const edit = ( { attributes, setAttributes } ) => {
         setAttributes( { content } );
     };
 
+    const onChangePreset = event => {
+        const preset      = event.target.value;
+        const foundPreset = presets.find( thePreset => thePreset.value === preset );
+
+        setAttributes( {
+            preset,
+            icon: foundPreset.icon,
+            type: foundPreset.type
+        } );
+    };
+
     return (
         <>
             <Fragment>
                 <InspectorControls>
-                    <PanelBody>
-                        <p>For a better icon preview visit <a href="https://heroicons.com" target={'_blank'}>HeroIcons</a>.</p>
-
-                        <PanelRow className = { 'wp-coutb-icon__panel-row' }>
-                            <div className = { 'wp-coutb-icon__container' }>
-                                <SelectControl
-                                    label = { 'Icon' }
-                                    value = { icon }
-                                    options = { icons }
-                                    onChange = { onChangeIcon }
-                                />
-
-                                <Icon icon={ icon } method={ method } />
-                            </div>
-                        </PanelRow>
-
-                        <PanelRow>
-                            <SelectControl
-                                label = { 'Type' }
-                                value = { type }
-                                options = { types }
-                                onChange = { onChangeType }
-                            />
-                        </PanelRow>
-
-                        <PanelRow>
-                            <SelectControl
-                                label = { 'Method' }
-                                value = { method }
-                                options = { [
+                    <Panel>
+                        <PanelBody title="Presets" initialOpen={ true }>
+                            <PanelRow>
+                                <div className="wp-coutb-presets">
                                     {
-                                        "label": "Solid",
-                                        "value": "solid"
-                                    },
-                                    {
-                                        "label": "Outline",
-                                        "value": "outline"
+                                        presets.map( ( preset, index ) => (
+                                            <label className={`wp-coutb-preset ${ preset.value }`}
+                                                   htmlFor={ preset.value }
+                                                   key={`${preset}-${index}`}
+                                            >
+                                                <input type="radio"
+                                                       id={ preset.value }
+                                                       value={ preset.value }
+                                                       name="wp-coutb-preset"
+                                                       onChange={ onChangePreset }
+                                                />
+                                                <Icon icon={ preset.icon } method="solid" />
+                                                <span>{ preset.label }</span>
+                                            </label>
+                                        ) )
                                     }
-                                ] }
-                                onChange = { onChangeMethod }
-                            />
-                        </PanelRow>
-                    </PanelBody>
+                                </div>
+                            </PanelRow>
+                        </PanelBody>
+                    </Panel>
+
+                    <Panel>
+                        <PanelBody title="Settings" initialOpen={ true }>
+                            <p>For a better icon preview visit <a href="https://heroicons.com" target={'_blank'}>HeroIcons</a>.</p>
+
+                            <PanelRow className = { 'wp-coutb-icon__panel-row' }>
+                                <div className = { 'wp-coutb-icon__container' }>
+                                    <SelectControl
+                                        label = { 'Icon' }
+                                        value = { icon }
+                                        options = { icons }
+                                        onChange = { onChangeIcon }
+                                    />
+
+                                    <Icon icon={ icon } method={ method } />
+                                </div>
+                            </PanelRow>
+
+                            <PanelRow>
+                                <SelectControl
+                                    label = { 'Type' }
+                                    value = { type }
+                                    options = { types }
+                                    onChange = { onChangeType }
+                                />
+                            </PanelRow>
+
+                            <PanelRow>
+                                <SelectControl
+                                    label = { 'Method' }
+                                    value = { method }
+                                    options = { [
+                                        {
+                                            "label": "Solid",
+                                            "value": "solid"
+                                        },
+                                        {
+                                            "label": "Outline",
+                                            "value": "outline"
+                                        }
+                                    ] }
+                                    onChange = { onChangeMethod }
+                                />
+                            </PanelRow>
+                        </PanelBody>
+                    </Panel>
                 </InspectorControls>
 
                 <div { ...useBlockProps() }>
